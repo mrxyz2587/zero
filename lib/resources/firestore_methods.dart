d454443d6552ff6012e916d8799c651eef501b05
Abhi Srivastava <zeromonkspad@gmail.com>
2022-09-03 10:32:58 +0530

    this is reel screen comment commit

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -170,6 +170,36 @@ class FireStoreMethods {
     return res;
   }
 
+  Future<String> reelsComment(String postId, String text, String uid,
+      String name, String profilePic) async {
+    String res = "Some error occurred";
+    try {
+      if (text.isNotEmpty) {
+        // if the likes list contains the user uid, we need to remove it
+        String commentId = const Uuid().v1();
+        _firestore
+            .collection('reels')
+            .doc(postId)
+            .collection('comments')
+            .doc(commentId)
+            .set({
+          'profilePic': profilePic,
+          'name': name,
+          'uid': uid,
+          'text': text,
+          'commentId': commentId,
+          'datePublished': DateTime.now(),
+        });
+        res = 'success';
+      } else {
+        res = "Please enter text";
+      }
+    } catch (err) {
+      res = err.toString();
+    }
+    return res;
+  }
+
   // Delete Post
   Future<String> deletePost(String postId) async {
     String res = "Some error occurred";
--- lib/screens/reels_screen.dart
+++ lib/screens/reels_screen.dart
@@ -1,10 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:video_player/video_player.dart';
+import 'package:zero_fin/screens/reels_comment_screen.dart';
 import 'package:zero_fin/widgets/video_player_items.dart';
 
 import '../providers/user_provider.dart';
+import '../resources/firestore_methods.dart';
+import '../utils/utils.dart';
+import '../widgets/like_animation.dart';
 
 class ReelsScreen extends StatefulWidget {
   const ReelsScreen({Key? key}) : super(key: key);
@@ -14,6 +19,24 @@ class ReelsScreen extends StatefulWidget {
 }
 
 class _ReelsScreenState extends State<ReelsScreen> {
+  int commentLen = 0;
+  // fetchCommentLen() async {
+  //   try {
+  //     QuerySnapshot snap = await FirebaseFirestore.instance
+  //         .collection('reels')
+  //         .doc()
+  //         .collection('comments')
+  //         .get();
+  //     commentLen = snap.docs.length;
+  //   } catch (err) {
+  //     showSnackBar(
+  //       context,
+  //       err.toString(),
+  //     );
+  //   }
+  //   setState(() {});
+  // }
+
   @override
   Widget build(BuildContext context) {
     final UserProvider userProvider = Provider.of<UserProvider>(context);
@@ -31,7 +54,11 @@ class _ReelsScreenState extends State<ReelsScreen> {
             ));
           }
           return PageView.builder(
+            pageSnapping: true,
+            controller: PageController(initialPage: 0, keepPage: false),
             scrollDirection: Axis.vertical,
+            allowImplicitScrolling: true,
+            onPageChanged: (val) {},
             itemCount: snapshot.data!.docs.length,
             itemBuilder: (ctx, index) => Container(
               height: MediaQuery.of(context).size.height,
@@ -39,7 +66,101 @@ class _ReelsScreenState extends State<ReelsScreen> {
               child: Stack(
                 children: [
                   VideoPlayerItem(
-                      videoUrl: snapshot.data!.docs[index].data()["reelUrl"])
+                      videoUrl: snapshot.data!.docs[index].data()["reelUrl"]),
+                  Positioned(
+                      top: 20,
+                      left: 10,
+                      child: Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              FontAwesomeIcons.arrowLeft,
+                              color: Colors.white,
+                            ),
+                            color: Colors.transparent,
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            "Social Wall",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700),
+                          )
+                        ],
+                      )),
+                  Positioned(
+                      bottom: 20,
+                      right: 20,
+                      child: Column(
+                        children: [
+                          LikeAnimation(
+                            isAnimating: snapshot.data!.docs[index]
+                                .data()['likes']
+                                .contains(userProvider.getUser.uid),
+                            smallLike: true,
+                            child: IconButton(
+                              icon: snapshot.data!.docs[index]
+                                      .data()['likes']
+                                      .contains(userProvider.getUser.uid)
+                                  ? const Icon(
+                                      FontAwesomeIcons.solidHeart,
+                                      color: Colors.red,
+                                    )
+                                  : const Icon(
+                                      FontAwesomeIcons.heart,
+                                      color: Colors.white,
+                                    ),
+                              onPressed: () => FireStoreMethods().likePost(
+                                snapshot.data!.docs[index]
+                                    .data()['reelId']
+                                    .toString(),
+                                userProvider.getUser.uid,
+                                snapshot.data!.docs[index].data()['likes'],
+                              ),
+                            ),
+                          ),
+                          Text(
+                            '${snapshot.data!.docs[index].data()['likes'].length} Likes',
+                            style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.white),
+                          ),
+                          SizedBox(width: 30),
+                          IconButton(
+                            color: Colors.transparent,
+                            icon: const Icon(
+                              Icons.comment_outlined,
+                              color: Colors.white,
+                            ),
+                            onPressed: () => Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => ReelsCommentsScreen(
+                                  postId: snapshot.data!.docs[index]
+                                      .data()['reelId']
+                                      .toString(),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Text(
+                            '0 comments',
+                            style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.white),
+                          ),
+                          SizedBox(width: 30),
+                          GestureDetector(
+                              child: Image.asset(
+                            'images/share_icons.png',
+                            height: 20,
+                          )),
+                        ],
+                      ))
                 ],
                 // snap: snapshot.data!.docs[index].data(),
               ),
